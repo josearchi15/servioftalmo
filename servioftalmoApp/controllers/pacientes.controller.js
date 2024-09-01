02b2cfff754a1c5c5a33bebf31a485392eb4f15e
josearchi15 <archibeis95@hotmail.com>
2024-09-01 14:10:08 -0600

    se corrige el manejo de la fecha para pasar al front

--- servioftalmoApp/controllers/pacientes.controller.js
+++ servioftalmoApp/controllers/pacientes.controller.js
@@ -15,39 +15,63 @@ export const getPacientes = async (req, res) => {
 
 export const getPaciente = async (req, res) => {
 
-    const pool = await getConnection()
-    const result = await pool.request().query(`SELECT * FROM GetPacienteByID(${req.params.id})`)
-    // console.log(result.recordset[0])
-    const Paciente = result.recordset[0]
-    console.log(typeof (result.recordset[0].Id_Paciente))
-    console.log(result.recordset[0])
-    // if (result.rowsAffected[0] == 0) {
-    //     return res.status(404).json({ message: "Paciente not found" })
-    // }
+    try {
+        const pool = await getConnection()
+        const result = await pool.request().query(`SELECT * FROM GetPacienteByID(${req.params.id})`)
+        const Paciente = result.recordset[0]
+        // console.log(Paciente)
+        const fecha = Paciente["Fecha_Nacimiento"]
+        const objFecha = JSON.stringify(fecha)
+        const fecha_front = objFecha.slice(1, 11)
+        console.log(fecha_front)
+        console.log(`Prueba: ${typeof (objFecha)}`)
 
-    res.render('paciente/editar-paciente', { Paciente: Paciente }) //{ Paciente: paciente }
+        res.render('paciente/editar-paciente', { Paciente: Paciente, Fecha_Nacimiento: fecha_front }) //{ Paciente: paciente }
+
+    } catch (error) {
+        console.log(error)
+        res.redirect("/pacientes/");
+    }
 
 }
 
 export const createPaciente = async (req, res) => {
     // const bday = Date(req.body.fechaNacimiento)
-    const pool = await getConnection()
-    const spCreatePaciente = await pool.request().query(
-        `EXEC spCreatePaciente '${req.body.nombres}','${req.body.apellidos}','${req.body.apellidoDeCasada}','${req.body.fechaNacimiento}','${req.body.No_Registro}',
-    	'${req.body.dpi}','${req.body.telCel}','${req.body.email}','${req.body.profesion}','${req.body.direccion}','${req.body.emergencyContact1}','${req.body.emergencyContact1Tel}','${req.body.emergencyContact2}','${req.body.emergencyContact1Tel}',
-    	'${req.body.diabetes} ${req.body.presionAltaBaja} ${req.body.enfermedadesCardiacas} ${req.body.doloresDeCabeza} ${req.body.asma} ${req.body.fracturas} ${req.body.convulsiones} ${req.body.problemasTorax}',
-        '${req.body.especifique}','${req.body.otrosAntecedentes}'`)
 
-    const spCreateDatosFacturacion = await pool.request().query(`EXEC spCreateDatosFacturacion '${req.body.facturacionNombre}','${req.body.facturacionNit}','${req.body.facturacionDireccion}'`)
+    try {
+        const pool = await getConnection()
+        const spCreatePaciente = await pool.request().query(
+            `EXEC spCreatePaciente '${req.body.nombres}','${req.body.apellidos}','${req.body.apellidoDeCasada}','${req.body.fechaNacimiento}','${req.body.No_Registro}',
+            '${req.body.dpi}','${req.body.telCel}','${req.body.email}','${req.body.profesion}','${req.body.direccion}','${req.body.emergencyContact1}','${req.body.emergencyContact1Tel}','${req.body.emergencyContact2}','${req.body.emergencyContact1Tel}',
+            '${req.body.diabetes} ${req.body.presionAltaBaja} ${req.body.enfermedadesCardiacas} ${req.body.doloresDeCabeza} ${req.body.asma} ${req.body.fracturas} ${req.body.convulsiones} ${req.body.problemasTorax}',
+            '${req.body.especifique}','${req.body.otrosAntecedentes}'`)
 
-    const spCreateDatosSeguro = await pool.request().query(`EXEC spCreateSeguroMedico '${req.body.seguroAfiliado}','${req.body.poliza}','${req.body.idAsegurado}','${req.body.noCarnet}'`)
-    res.redirect("/pacientes/");
+        const spCreateDatosFacturacion = await pool.request().query(`EXEC spCreateDatosFacturacion '${req.body.facturacionNombre}','${req.body.facturacionNit}','${req.body.facturacionDireccion}'`)
+
+        const spCreateDatosSeguro = await pool.request().query(`EXEC spCreateSeguroMedico '${req.body.seguroAfiliado}','${req.body.poliza}','${req.body.idAsegurado}','${req.body.noCarnet}'`)
+        res.redirect("/pacientes/");
+
+    } catch (error) {
+        console.log(error)
+        res.redirect("/");
+    }
 }
 
 export const updatePaciente = async (req, res) => {
     try {
         const pool = await getConnection()
-        const result = await pool.request().query(`EXEC spDeletePacienteById ${req.params.id}`)
+        const updatePaciente = await pool.request().query(
+            `EXEC spUpdatePacienteById ${req.params.id},'${req.body.nombres}','${req.body.apellidos}','${req.body.apellidoDeCasada}','${req.body.fechaNacimiento}','${req.body.No_Registro}',
+            '${req.body.dpi}','${req.body.telCel}','${req.body.email}','${req.body.profesion}','${req.body.direccion}','${req.body.emergencyContact1}','${req.body.emergencyContact1Tel}','${req.body.emergencyContact2}','${req.body.emergencyContact1Tel}',
+            '${req.body.diabetes} ${req.body.presionAltaBaja} ${req.body.enfermedadesCardiacas} ${req.body.doloresDeCabeza} ${req.body.asma} ${req.body.fracturas} ${req.body.convulsiones} ${req.body.problemasTorax}',
+            '${req.body.especifique}','${req.body.otrosAntecedentes}'`)
+
+        const updateDatosFacturacion = await pool.request().query(
+            `EXEC spUpdateDatosFacturacionByPacienteId ${req.params.id},'${req.body.facturacionNombre}','${req.body.facturacionNit}','${req.body.facturacionDireccion}'`)
+
+        const udpateSeguroMedico = await pool.request().query(
+            `EXEC spUpdateSeguroMedicoByPacienteID ${req.params.id},'${req.body.facturacionNombre}','${req.body.facturacionNit}','${req.body.facturacionDireccion}'`)
+
         res.redirect("/pacientes/");
 
     } catch (error) {
